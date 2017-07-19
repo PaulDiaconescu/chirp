c2febea6794e566dbe04d0133e0fd292e413465e
PaulDiaconescu <diaconescu.paul11@gmail.com>
2017-07-19 12:19:53 +0300

    Added css to register and login page

--- message/views.py
+++ message/views.py
@@ -7,15 +7,16 @@ from django.views.generic.edit import CreateView
 from django.views.generic.list import ListView
 
 from message.forms import RegisterForm
+from message.models import Message
 
 
 class RegisterView(CreateView):
     form_class = RegisterForm
     template_name = 'register.html'
-    success_url = "/"
+    success_url = "/login"
 
 class TimelineView(ListView):
     template_name = 'index.html'
     def get_queryset(self):
         # implement the logic
-        pass
+        return Message.objects.all()
