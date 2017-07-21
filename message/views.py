8b59bd78655a3f26317d3892054f37404e3074d5
PaulDiaconescu <diaconescu.paul11@gmail.com>
2017-07-21 23:55:41 +0300

    Added profile views

--- message/views.py
+++ message/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render
-
 # Create your views here.
+from django.contrib.auth.models import User
 from django.shortcuts import render
 
+from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView
 from django.views.generic.list import ListView
 
@@ -23,3 +23,20 @@ class TimelineView(ListView):
             return Message.objects.filter(user=self.request.user)
         else:
             return Message.objects.all()
+
+class ProfileBaseView(DetailView):
+    model = User
+    template_name = 'profile.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(ProfileBaseView, self).get_context_data(**kwargs)
+        context["chirps"] = Message.objects.filter(user=self.get_object())
+        return context
+
+class MyProfileView(ProfileBaseView):
+    def get_object(self, queryset=None):
+        return self.request.user
+
+class ProfileView(ProfileBaseView):
+    def get_slug_field(self):
+        return "username"
